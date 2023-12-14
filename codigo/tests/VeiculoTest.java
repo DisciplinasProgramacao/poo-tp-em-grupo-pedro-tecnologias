9b0c9c8ea518a7828585b84ae78b5cedc6551273
Samuel Van Petten <104178768+Samuelrcvp@users.noreply.github.com>
2023-12-13 23:41:40 -0300

    Update VeiculoTest.java

--- codigo/tests/VeiculoTest.java
+++ codigo/tests/VeiculoTest.java
@@ -9,60 +9,64 @@ import java.util.Date;
 
 public class VeiculoTest {
 
-    private Veiculo veiculo;
-    private Veiculo veiculo2;
+    private Carro carro;
     private Rota rota1;
     private Rota rota2;
 
     @Before
     public void setUp() {
-        veiculo = new Carro("ABC1234", ECombustivel.GASOLINA);
-        veiculo2 = new Carro("EFG9876", ECombustivel.GASOLINA);
+        Frota frota = new Frota();
+        carro = new Carro("ABC1234", ECombustivel.GASOLINA);
+        frota.adicionarVeiculo(carro);
         rota1 = new Rota(20.0, new Date());
         rota2 = new Rota(40.0, new Date());
-        veiculo.abastecer(100);
-        veiculo2.abastecer(5);
-        
     }
 
     @Test
-    public void testPercorrerRota() throws ParseException{
-        veiculo.percorrerRota(rota1);
-        assertEquals(48.0, veiculo.tanque.getCapacidadeAtual(),0.01);
+    public void testPercorrerRotaAbastecimentoAutomatico() {
+        // Autonomia atual insuficiente para a rota
+        assertTrue(carro.percorrerRota(rota1));
+        // Autonomia é abastecida automaticamente
+        assertEquals(1, carro.qtdRotasPercorridas());
     }
 
     @Test
-    public void testAbastecer() {
-        double litrosAbastecidos = veiculo2.abastecer(10.0) + veiculo2.abastecer(10.0);
+    public void testPercorrerRota(){
+        Frota frota = new Frota();
+        carro = new Carro("ABC1234", ECombustivel.GASOLINA);
+        frota.adicionarVeiculo(carro);
+        rota1 = new Rota(20.0, new Date());
+        rota2 = new Rota(40.0, new Date());
 
-        assertEquals(20.0, litrosAbastecidos, 0.01);
-        assertEquals(25.0, veiculo2.tanque.getCapacidadeAtual(), 0.01);
+        carro.percorrerRota(rota1);
+        assertEquals(0, carro.tanque.getCapacidadeAtual(),0.01);
     }
 
     @Test
     public void testKmNoMes() throws ParseException {
-        veiculo.percorrerRota(rota1);
-        assertEquals(20.0, veiculo.kmNoMes(), 0.01);
+        
+        carro.percorrerRota(rota1);
+        assertEquals(20.0, carro.kmNoMes(), 0.01);
 
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.MONTH, -1);
         Rota rotaForaDoMes = new Rota(30.0, cal.getTime());
-        veiculo.percorrerRota(rotaForaDoMes);
+        carro.percorrerRota(rotaForaDoMes);
 
-        assertEquals(20.0, veiculo.kmNoMes(), 0.01);
+        assertEquals(20.0, carro.kmNoMes(), 0.01);
     }
 
     @Test
     public void testKmTotal() throws ParseException {
-        veiculo.percorrerRota(rota1);
-        veiculo.percorrerRota(rota2);
-        assertEquals(60.0, veiculo.kmTotal(), 0.01);
+        carro.percorrerRota(rota1);
+        carro.percorrerRota(rota2);
+        assertEquals(60.0, carro.kmTotal(), 0.01);
     }
 
     @Test
     public void testPlacaCorresponde() {
-        assertTrue(veiculo.placaCorresponde("ABC1234"));
-        assertFalse(veiculo.placaCorresponde("XYZ5678"));
+        assertTrue(carro.placaCorresponde("ABC1234"));
+        assertFalse(carro.placaCorresponde("XYZ5678"));
     }
 
 
@@ -72,24 +76,23 @@ public class VeiculoTest {
 
         Veiculo veiculo3 = new Carro("GFS1234", ECombustivel.GASOLINA);
 
-        veiculo3.abastecer(45);
+        Rota rota3 = new Rota(250.0, new Date());
+        Rota rota4 = new Rota(300.0, new Date());
 
-        veiculo3.percorrerRota(rota1);//20
-        veiculo3.percorrerRota(rota2);//40
-        veiculo3.abastecer(20.0);
-        veiculo3.abastecer(10.0);
+        veiculo3.percorrerRota(rota3);
+        veiculo3.percorrerRota(rota4);
 
         // Teste do método toString
         String expectedToString = "\n=============== VEÍCULO ==============="
                 + "\nPlaca: GFS1234"
-                + "\nTanque Maxímo: 50 | Tanque Atual: 50"
-                + "\nTotal já abastecido: 56"
+                + "\nTanque Maxímo: 50 | Tanque Atual: 0"
+                + "\nTotal já abastecido: 55"
                 + "\nRotas percorridas: 2"
-                + "\nQuilometragem total: 60"
-                + "\nQuilometragem do mês: 60"
-                + "\nProxima manutenção periódica daqui a 10000.0 km"
-                + "\nProxima troca de peças daqui a 10000.0 km"
-                + "\nDespeza total: R$ 233,55"
+                + "\nQuilometragem total: 550"
+                + "\nQuilometragem do mês: 550"
+                + "\nProxima manutenção periódica daqui a 9450.0 km"
+                + "\nProxima troca de peças daqui a 9450.0 km"
+                + "\nDespeza total: R$ 285,45"
                 + "\n";
         assertEquals(expectedToString, veiculo3.toString());
     }
