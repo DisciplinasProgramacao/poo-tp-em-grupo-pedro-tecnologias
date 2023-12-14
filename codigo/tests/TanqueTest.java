5714a02a1079743d873f90edf8f587bcaf6c8653
Samuel Van Petten <104178768+Samuelrcvp@users.noreply.github.com>
2023-12-13 23:41:20 -0300

    Update TanqueTest.java

--- codigo/tests/TanqueTest.java
+++ codigo/tests/TanqueTest.java
@@ -1,45 +1,71 @@
-import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class TanqueTest {
 
-    @Test
-    public void testAbastecerComTanqueVazio() {
-        Tanque tanque = new Tanque(EVeiculo.CARRO.capacidadeMaximaTanque, ECombustivel.GASOLINA.consumoMedio, ECombustivel.GASOLINA.precoMedio);
-        double litrosAbastecidos = tanque.abastecer(20.0);
-        assertEquals(20.0, litrosAbastecidos, 0.01);
-        assertEquals(20.0, tanque.getCapacidadeAtual(), 0.01);
+    private Tanque tanque;
+
+    @Before
+    public void setUp() {
+        EVeiculo tipoVeiculo = EVeiculo.CARRO;
+        ECombustivel tipoCombustivel = ECombustivel.GASOLINA;
+
+        tanque = new Tanque(tipoVeiculo, tipoCombustivel);
     }
 
     @Test
-    public void testAbastecerComTanqueCheio() {
-        Tanque tanque = new Tanque(EVeiculo.CARRO.capacidadeMaximaTanque, ECombustivel.GASOLINA.consumoMedio, ECombustivel.GASOLINA.precoMedio);
-        tanque.abastecer(50.0);
-        double litrosAbastecidos = tanque.abastecer(20.0);
-        
-        assertEquals(0d, litrosAbastecidos, 0.01);
-        assertEquals(50d, tanque.getCapacidadeAtual(), 0.01);
+    public void testGastarCombustivel() {
+        tanque.abastecer(50);
+        double litrosGastos = tanque.gastarCombustivel(20);
+        assertEquals(20, litrosGastos, 0.01);
+        assertEquals(30, tanque.getCapacidadeAtual(), 0.01);
     }
 
     @Test
-    public void testAbastecerComTanqueParcialmenteCheio() {
-        Tanque tanque = new Tanque(EVeiculo.CARRO.capacidadeMaximaTanque, ECombustivel.GASOLINA.consumoMedio, ECombustivel.GASOLINA.precoMedio);
-        tanque.abastecer(25);
-        double litrosAbastecidos = tanque.abastecer(20.0);
-        assertEquals(20d, litrosAbastecidos, 0.01);
-        assertEquals(45d, tanque.getCapacidadeAtual(), 0.01);
+    public void testAbastecerTanqueVazio() {
+
+        double litrosAbastecidos = tanque.abastecer(30);
+        assertEquals(30, litrosAbastecidos, 0.01);
+        assertEquals(30, tanque.getCapacidadeAtual(), 0.01);
+        assertEquals(30, tanque.getTotalAbastecido(), 0.01);
+    }
+
+    @Test
+    public void testAbastecerTanqueCheio() {
+        tanque.abastecer(50);
+        double litrosAbastecidos = tanque.abastecer(30);
+        assertEquals(-1, litrosAbastecidos, 0.01);
+        assertEquals(50, tanque.getCapacidadeAtual(), 0.01);
+        assertEquals(50, tanque.getTotalAbastecido(), 0.01);
+    }
+
+    @Test
+    public void testAbastecerTanqueParcialmenteCheio() {
+        tanque.abastecer(20);
+        double litrosAbastecidos = tanque.abastecer(30);
+        assertEquals(30, litrosAbastecidos, 0.01);
+        assertEquals(50, tanque.getCapacidadeAtual(), 0.01);
+        assertEquals(50, tanque.getTotalAbastecido(), 0.01);
+    }
+
+    @Test
+    public void testValorGastoCombustivel() {
+        tanque = new Tanque(EVeiculo.CARRO, ECombustivel.GASOLINA);
+        tanque.abastecer(40);
+        double valorGasto = tanque.valorGastoCombustivel();
+        assertEquals(207.60, valorGasto, 0.01);
     }
 
     @Test
     public void testAutonomiaMaxima() {
-        Tanque tanque = new Tanque(EVeiculo.CARRO.capacidadeMaximaTanque, ECombustivel.GASOLINA.consumoMedio, ECombustivel.GASOLINA.precoMedio);
-        assertEquals(50d * 10d, tanque.autonomiaMaxima(), 0.01);
+        assertEquals(500, tanque.getAutonomiaMaxima(), 0.01);
     }
 
     @Test
     public void testAutonomiaAtual() {
-        Tanque tanque = new Tanque(EVeiculo.CARRO.capacidadeMaximaTanque, ECombustivel.GASOLINA.consumoMedio, ECombustivel.GASOLINA.precoMedio);
         tanque.abastecer(30);
-        assertEquals(30.0 * 10d, tanque.autonomiaAtual(), 0.01);
+        assertEquals(300, tanque.getAutonomiaAtual(), 0.01);
     }
 }
